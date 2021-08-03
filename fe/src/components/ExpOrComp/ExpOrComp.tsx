f3ef2af9fbd917ad5fb4bd0c57b3df11893ddbd4
Gabriele Corti <gabriele.corti.lavoro@gmail.com>
2021-08-03 11:41:53 +0200

    fix: UI for Protected route

--- fe/src/components/ExpOrComp/ExpOrComp.tsx
+++ fe/src/components/ExpOrComp/ExpOrComp.tsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import { Page, RegisterBox, Title } from '../Register/Style'
-import { RootStateOrAny, useSelector } from 'react-redux'
-import {GoToRegister, StyledLink} from '../Login/Style'
+import React from "react";
+import { Page, RegisterBox, Title } from "../Register/Style";
+import { RootStateOrAny, useSelector } from "react-redux";
+import { GoToRegister, StyledLink } from "../Login/Style";
 
+const ExpOrComp: React.FC = ({ children }) => {
+  const Token = localStorage.getItem("token");
+  const Name = useSelector((state: RootStateOrAny) => state.loginReducer.name);
 
-const ExpOrComp:React.FC = ({children}) => {
-    const Token = localStorage.getItem('token');
-    const Name = useSelector((state:RootStateOrAny) => state.loginReducer.name)
-
-
-    return (
-        <Page>   
-            {
-                Token && Name ?
-                children :
-                <RegisterBox>
-                <Title>Spiacente ma non ti è permesso entrare qui</Title>
-                <GoToRegister>procedi con il <StyledLink to='/login'>Login</StyledLink> o <StyledLink to='/register'>Registrati</StyledLink></GoToRegister>
-                </RegisterBox> 
-            }
+  return (
+    <>
+      {Token && Name ? (
+        children
+      ) : (
+        <Page>
+          <RegisterBox>
+            <Title>Spiacente ma non ti è permesso entrare qui</Title>
+            <GoToRegister>
+              procedi con il <StyledLink to="/login">Login</StyledLink> o{" "}
+              <StyledLink to="/register">Registrati</StyledLink>
+            </GoToRegister>
+          </RegisterBox>
         </Page>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default ExpOrComp
+export default ExpOrComp;
